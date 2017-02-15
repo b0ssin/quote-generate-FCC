ee4b8707996ed3796f8b14d31053501cc5635858
b0ssin <krillinmaster@gmail.com>
2017-02-15 14:41:29 -0500

    getQuote on start-up and added media queries for mobile

--- quote.js
+++ quote.js
@@ -1,50 +1,35 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    let getQuote = function() {
+        let request = new XMLHttpRequest();
+        let url = "http://api.icndb.com/jokes/random"
+        request.open("GET", url, true);
+        request.onreadystatechange = function() {
+            if (request.status >= 200 && request.status < 400) {
+                let data = JSON.parse(request.responseText);
+                // Connected
+                console.log(data);
+                console.log(data.value.joke);
+                let quote = data.value.joke;
+                let quoteElement = document.getElementById("quote");
+                // Updating quote
+                quoteElement.innerHTML = quote;
+            } else {
+                console.log("reached server then got an error")
+            }
+        };
+
+        request.onerror = function() {
+            console.log("Connection error");
+        }
+
+        request.send();
+    };
+
+    getQuote();
+
     let body = document.getElementById("body");
     let container = body.childNodes[1];
     let button = container.children[0];
-    button.addEventListener("click", function() {
-            let request = new XMLHttpRequest();
-            let url = "http://api.icndb.com/jokes/random"
-            request.open("GET", url, true);
-            request.onreadystatechange = function() {
-                if (request.status >= 200 && request.status < 400) {
-                    let data = JSON.parse(request.responseText);
-                    // Connected
-                    console.log(data);
-                    console.log(data.value.joke);
-                } else {
-                    console.log("reached server then got an error")
-                }
-            };
-
-            request.onerror = function() {
-                console.log("Connection error");
-            }
-
-            request.send();
-        })
-        // button.onclick = function() {
-        //     let request = new XMLHttpRequest();
-        //     let url = "http://api.icndb.com/jokes/random"
-        //     request.open("GET", url, true);
-        //     request.onreadystatechange = function() {
-        //         if (request.status >= 200 && request.status < 400) {
-        //             let data = JSON.parse(request.responseText);
-        //             // Connected
-        //             console.log(data);
-        //             console.log(data.value.joke);
-        //         } else {
-        //             console.log("reached server then got an error")
-        //         }
-        //     };
-
-    //     request.onerror = function() {
-    //         console.log("Connection error");
-    //     }
-
-    //     request.send();
-
-
-    // }
+    button.addEventListener("click", getQuote);
 })
